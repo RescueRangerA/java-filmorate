963eca2263919d8aa56596031bd50c1f540711f6
edward <hammer_head@mail.ru>
2022-10-27 10:43:57 +0400

    fix: add missing @Nullable and @NotNull annotations

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.lang.Nullable;
 import ru.yandex.practicum.filmorate.validation.constraints.AfterTheDate;
 
 import javax.validation.constraints.*;
@@ -12,17 +13,22 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 public class Film {
     @Positive
+    @Nullable
     private Long id;
 
     @NotBlank
+    @NotNull
     private String name;
 
     @Size(max = 200)
+    @NotNull
     private String description;
 
     @AfterTheDate(message = "Release date should not be less 1895-12-28", moment = "1895-12-28")
+    @NotNull
     private LocalDate releaseDate;
 
     @Positive
+    @NotNull
     private Integer duration;
 }
