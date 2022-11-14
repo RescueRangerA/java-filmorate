08c5ccbeffca3a18c076d44bfc65f6d5da1fab45
edward <hammer_head@mail.ru>
2022-11-14 21:16:45 +0400

    feat: add Exception handler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionHandler.java
@@ -2,11 +2,15 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.log.LogFormatUtils;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 import ru.yandex.practicum.filmorate.storage.EntityAlreadyExistsException;
 import ru.yandex.practicum.filmorate.storage.EntityIsNotFoundException;
 import ru.yandex.practicum.filmorate.storage.userfriend.FriendOfHisOwnException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -35,6 +39,24 @@ public class ExceptionHandler {
     public void handleValidationException(final FriendOfHisOwnException e, HttpServletResponse response) throws IOException {
         logIfNeeded(e);
         response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ModelAndView handleValidationException(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @Nullable Object handler,
+            Exception e
+    ) throws IOException {
+        ModelAndView mav = (new DefaultHandlerExceptionResolver()).resolveException(request, response, handler, e);
+
+        if (mav != null) {
+            return mav;
+        }
+
+        logIfNeeded(e);
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        return new ModelAndView();
     }
 
     protected void logIfNeeded(Exception e) {
