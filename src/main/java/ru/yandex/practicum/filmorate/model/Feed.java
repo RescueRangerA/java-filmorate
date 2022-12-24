4802b12e2e961a40e92158cb79816c8e2e43721c
Александр <a.o.engovatov@yandex.ru>
2022-12-24 17:48:14 +0400

    добавление Feed, добавление Enum OperationType, Enum EventType, добавлен эндпоинт /{id}/feed

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feed {
+
+    @Positive
+    @Nullable
+    private Long id;
+
+    private Long timeStamp;
+
+    @Positive
+    @Nullable
+    private Long userId;
+
+    @NotBlank
+    @NotNull
+    private Enum<EventType> eventType;
+
+    @NotBlank
+    @NotNull
+    private Enum<OperationType> operationType;
+
+    @Positive
+    @NotNull
+    private Integer entityId;
+
+    public Feed(
+            @Nullable Long id,
+            Long userId,
+            Enum<EventType> eventType,
+            Enum<OperationType> operationType,
+            Integer entityId
+    ) {
+        this.id = id;
+        this.userId = userId;
+        this.eventType = eventType;
+        this.operationType = operationType;
+        this.entityId = entityId;
+        this.timeStamp = System.currentTimeMillis()/1000;
+    }
+}
